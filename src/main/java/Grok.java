72420e313573cbad41c63d985276463f6148d3fa
Bacon <parkerjoshua0403@gmail.com>
2026-03-02 11:19:28 -0500

    Assignment Completed. Issues: remembering to use the method in takePowerPill (pill.getPower()), and making sure the name of tookHit was correct.

--- src/main/java/Grok.java
+++ src/main/java/Grok.java
@@ -7,7 +7,63 @@
  * @author (You Again)
  * @version (0.1)
  */
-public class Grok
-{
-    // implement this class per instructions in Required Activities.
+public class Grok {
+    private static final int DEFAULT_POWER_LEVEL = 50;
+
+    // instance variables
+    private int powerLevel;
+
+    // constructors
+    /**
+     \* Initializes a Grok object to the default power level of 50.
+     */
+    public Grok() {
+        setPowerLevel(DEFAULT_POWER_LEVEL);
+    }
+
+    /**
+     *
+     * Initializes a Grok object to power powerLevel
+     */
+    public Grok(int powerLevel) {
+        this.powerLevel = powerLevel;
+    }
+
+    // getter methods
+    /**
+     * Returns the power level of this Grok.
+     * @return returns the power level of this Grok
+     */
+    public int getPowerLevel() {
+        return powerLevel;
+    }
+
+    // setter methods
+    /**
+     * Sets the power level of this Grok.
+     * @param powerLevel the power value to set for this Grok.
+     */
+    public void setPowerLevel(int powerLevel) {
+        this.powerLevel = powerLevel;
+    }
+
+    // mutator methods
+
+    /**
+     * Set the power level of this Grok to the power level of
+     * the pill.
+     * @param pill The PowerPill that this Grok took.  The power
+     * of the pill is added to the power level of this Grok.
+     */
+    public void takePowerPill(PowerPill pill) {
+        powerLevel += pill.getPower();
+    }
+
+    /**
+     * Invoked when this Grok takes a hit.  The power level of
+     * this Grok is reduced by 5.
+     */
+    public void tookHit(){
+        powerLevel -= 5;
+    }
 }
